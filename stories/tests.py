45ecc8294428d2fb0b602e2643d90214e646173d
Skye Im <skye.im@nyu.edu>
2018-12-20 16:39:38 -0500

    Meta: Test framework for stories, no tests

--- stories/tests.py
+++ stories/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django_webtest import WebTest
+from django.contrib.auth.models import User
+from profiles.models import UserProfile
+from .models import Story, Chapter
 
-# Create your tests here.
+
+class TestStory(WebTest):
+    chapter1 = None
+
+    def setUp(self):
+        user = G(User, username='test-user')
+        profile = G(UserProfile, user=user, screenname='Tester')
+        story = G(Story,
+            owner=user,
+            worksummary='This is a <b>work</b> summary.',
+            worktitle='Test Saga')
+        self.chapter1 = G(Chapter,
+            parent=story,
+            author=user,
+            chaptertext='The content of a <b>chapter</b>.',
+            chaptertitle='Chapter Title')
+        G(Chapter,
+            parent=story,
+            author=user,
+            chaptertext='The content of chapter <em>two</em>.',
+            chaptertitle='Second chapter title',
+            chaptersummary='Summary for <u>chapter</u> two.',
+            chapterorder=1)
