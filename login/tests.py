e23372f6bf343fec49ec4859455c9b7870737f8c
Skye Im <skye.im@nyu.edu>
2018-12-21 21:19:54 -0500

    Fix: login test remove useless arg

--- login/tests.py
+++ login/tests.py
@@ -4,8 +4,8 @@ from profiles.models import UserProfile
 
 
 class TestRegistration(WebTest):
-    def try_signup(self, app):
-        res = app.get('/login/signup')
+    def try_signup(self):
+        res = self.app.get('/login/signup')
         form = res.form
         form['screenname'] = 'Alexander Hamilton'
         form['loginname'] = 'hamilton'
@@ -18,14 +18,14 @@ class TestRegistration(WebTest):
 
     # Check that a user is actually made when signup
     def test_signup(self):
-        self.try_signup(self.app)
+        self.try_signup()
         try:
             User.objects.get(username='hamilton')
         except User.DoesNotExist:
             self.fail('User was not created')
 
     def test_profile_created(self):
-        self.try_signup(self.app)
+        self.try_signup()
         try:
             UserProfile.objects.get(screenname='Alexander Hamilton')
         except UserProfile.DoesNotExist:
@@ -33,7 +33,7 @@ class TestRegistration(WebTest):
 
     # Check that user is redirected to front page after signup
     def test_redirect(self):
-        res = self.try_signup(self.app)
+        res = self.try_signup()
         '''
         At the moment, only checks that it redirects at all.
         TODO: Test that the following page is, indeed, the front page.
